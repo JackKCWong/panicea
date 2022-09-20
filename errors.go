9141de97abb81a91d75194dc364e1ca9627da4cd
JackKCWong <jiajue.huang@live.com>
2022-09-21 07:44:33 +0800

    add Try & Trap

--- errors.go
+++ errors.go
@@ -18,13 +18,28 @@ func Check(err error) {
 	}
 }
 
+func Try(fn func()) (reErr error) {
+	defer Catch(&reErr, nil)
+	fn()
+
+	return nil
+}
+
+func Trap[T any](fn func() T) (val T, reErr error) {
+	defer Catch(&reErr, nil)
+
+	return fn(), nil
+}
+
 func Catch(cause *error, handler func(error)) {
 	if caught := recover(); caught != nil {
 		if err, ok := caught.(error); ok {
-			*cause = err 
-			handler(err)
+			*cause = err
+			if handler != nil {
+				handler(err)
+			}
 		} else {
-			panic(fmt.Errorf("expecint an error but was: %q", caught))
+			panic(fmt.Errorf("expecting a error type but was: %q", caught))
 		}
 	}
 }
--- errors_test.go
+++ errors_test.go
@@ -3,7 +3,6 @@ package panicea
 import (
 	"fmt"
 	"testing"
-
 )
 
 func TestCheck(t *testing.T) {
@@ -19,13 +18,41 @@ func TestCheck(t *testing.T) {
 	}()
 
 	if err.Error() != "I am an error" {
-		t.Logf("unexpected error: %+v", err)
-		t.FailNow()
+		t.Fatalf("unexpected error: %+v", err)
 	}
 
 	if !errorCaught {
-		t.Log("error handler not called")
-		t.FailNow()
+		t.Fatal("error handler not called")
+	}
+
+	t.Logf("%+v", err)
+}
+
+func TestTrap(t *testing.T) {
+	n, err := Trap(func() int {
+		return 10
+	})
+
+	if err != nil {
+		t.Fatalf("unexpected error: %q", err)
+	}
+
+	if n != 10 {
+		t.Fatalf("unexpected result: %q", n)
+	}
+
+	n, err = Trap(func() int {
+		Check(fmt.Errorf("I am an error"))
+
+		return -1
+	})
+
+	if err == nil || err.Error() != "I am an error" {
+		t.Fatalf("unexpected error: %q", err)
+	}
+
+	if n != 0 {
+		t.Fatalf("unexpected result: %q", n)
 	}
 
 	t.Logf("%+v", err)
